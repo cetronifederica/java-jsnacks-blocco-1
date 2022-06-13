c45ad9fe3c1db9ad6c1808b59076bc11ff105733
federicacetroni <cetronifederica97@gmail.com>
2022-06-13 10:34:48 +0200

    primo jsnacks con ciclo for

---
+++ src/jsnacks/JSnacks1.java
@@ -0,0 +1,37 @@
+package jsnacks;
+
+import java.util.Scanner;
+
+public class JSnacks1 {
+
+	public static void main(String[] args) {
+		/* Il software deve chiedere per 5 volte all’utente di inserire un numero.
+		Il programma stampa la somma di tutti i numeri inseriti.
+		Esegui questo programma in due versioni, con il for e con il while.*/
+		
+		
+		Scanner numUtente = new Scanner(System.in);
+		int N = 5; // valori da sommare alla fine
+        int conta = 1;   // conta i valori inseriti dall'utente
+        int somma = 0;   // somma dei valori letti
+        int num;         // ultimo valore letto 
+
+		
+		
+		
+		 // chiedo all'utente di inserire un numero x 5 volte 
+		
+        for (conta =1; conta <= N; conta++) {
+            System.out.print("Immetti un intero: ");
+            num = numUtente.nextInt();
+            somma = somma + num;
+        }
+        
+        System.out.println ("La somma dei tuoi numeri è: " + somma);
+        
+        
+        numUtente.close();
+
+	}
+
+}
