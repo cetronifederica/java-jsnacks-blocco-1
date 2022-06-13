9f95cd6efef74459f45a33651450efc7d382ae51
federicacetroni <cetronifederica97@gmail.com>
2022-06-13 11:32:52 +0200

    inserito esercio jsnacks2

---
+++ src/jsnacks/JSnacks2.java
@@ -0,0 +1,29 @@
+package jsnacks;
+
+import java.util.Scanner;
+
+public class JSnacks2 {
+
+	public static void main(String[] args) {
+		/*Chiedi all'utente di inserire un numero, se è pari stampa il numero,
+		 * se è dispari stampa il numero successivo */
+
+		Scanner numUtente = new Scanner(System.in);
+		int num;
+		
+		// chiedo all'utente di inserire un numero
+		
+			System.out.println("Immetti un numero: ");
+			num = numUtente.nextInt();
+			if (num % 2 == 0) {
+				System.out.println(num);
+			} else {
+				System.out.println(num+1);
+			}
+			numUtente.close();
+		}
+		
+		
+		
+	}
+
---
+++ src/jsnacks/JSnacks3.java
@@ -0,0 +1,14 @@
+package jsnacks;
+
+public class JSnacks3 {
+
+	public static void main(String[] args) {
+		/*Generatore di “nomi cognomi” casuali: prendendo una lista di nomi e una
+		 * lista di cognomi, genera una falsa lista di 3 invitati*/
+
+		
+		String [] nomi = {"Federica" , "Andrea" , "Giulia" , "Matteo" , "Marco"};
+		String [] cognomi = {"Cetroni", "Tarantino", "Cavalca", "Tarquini", "Belli"};
+	}
+
+}
